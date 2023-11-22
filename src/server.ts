494cc82758cb1e5192172a2cbbf20f403922735e
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-11-22 19:07:59 -0300

    configurar lint e formatador de codigo

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 const app = fastify();
 
-app.get("/", () => {
-  return "Hello World";
+app.get('/', () => {
+  return 'Hello World';
 });
 
 app
   .listen({
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 3333,
   })
   .then(() => {
-    console.log("HTTP Server running on http://localhost:3333");
+    console.log('HTTP Server running on http://localhost:3333');
   });
