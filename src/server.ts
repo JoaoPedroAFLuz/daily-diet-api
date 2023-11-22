8e5300f543a6a930761e0328c4a78a11e8e07849
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-11-22 20:47:37 -0300

    configurar variaveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify';
+import { randomUUID } from 'node:crypto';
 
 import { knex } from './database';
+import { env } from './env';
 
 const app = fastify();
 
@@ -10,11 +12,25 @@ app.get('/meals', async () => {
   return tables;
 });
 
+app.post('/meals', async (req, res) => {
+  const transaction = await knex('meals')
+    .insert({
+      id: randomUUID(),
+      name: 'Hamburger',
+      description: 'Sandwich',
+      dateTime: new Date().toISOString(),
+      isDiet: false,
+    })
+    .returning('*');
+
+  return res.status(201).send({ transaction });
+});
+
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('HTTP Server running on http://localhost:3333');
+    console.log(`HTTP Server running on http://localhost:${env.PORT}`);
   });
